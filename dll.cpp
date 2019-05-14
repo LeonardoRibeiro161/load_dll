af66c03e2654cbeb1105f182f7f659dfb63c6037
LeonardoRibeiro161 <48728011+LeonardoRibeiro161@users.noreply.github.com>
2019-05-14 17:39:50 -0300

    Add files via upload

---
+++ dll.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "windows.h"
+ 
+typedef int (__stdcall* stcall_t)(void);
+ 
+void call_dll(char *name_dll, char *function){
+ 
+  HMODULE path_dll = LoadLibrary(name_dll);
+  if(path_dll == NULL){
+    exit(1);
+  }
+ 
+  stcall_t search = (stcall_t)GetProcAddress(path_dll, function);
+  search();
+  FreeLibrary(path_dll);
+}
+ 
+int main(int argc, char *argv[]){
+  if(argc > 2)
+    call_dll(argv[1], argv[2]);
+ 
+  return 0;
+}
